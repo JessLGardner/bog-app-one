7c99a7f44ced1003fce821c2e2493e609c893582
Jess Gardner <inanna1130@gmail.com>
2017-09-06 14:09:28 -0400

    Added edit and update routes

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -19,6 +19,23 @@ class CreaturesController < ApplicationController
         @creature = Creature.find(params[:id])
     end
 
+    def edit
+        @creature = Creature.find(params[:id])
+    end
+
+    def update
+        @creature = Creature.find(params[:id])
+        @creature.update(creature_params)
+        
+        if @creature.save
+            redirect_to creature_path(@creature.id)
+        end
+    end
+
+    def destroy
+
+    end
+
 
 
     private
